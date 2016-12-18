4a320c07386d82ffe0fcf95d55f217c69fe1fd4f
Alessandro Desantis <desa.alessandro@gmail.com>
2016-12-18 23:59:03 +0100

    Implement Devise::Models::Confirmable

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 # To do
 
-- Devise modules
-- Mailer layout
-- Pagination
-- Serialization (?)
+- Implement `Devise::Models::Recoverable`
+- Respond with specific error in `ConfirmationsController#update`
+- Create mailer layout
+- Implement pagination
+- Implement serialization (?)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :pings, only: %i(create)
       resources :tokens, only: %i(create)
+      resources :confirmations, only: %i(create) do
+        member do
+          post :complete
+        end
+      end
     end
   end
 end
