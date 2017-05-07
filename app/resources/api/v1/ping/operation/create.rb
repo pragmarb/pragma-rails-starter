06340b6229865b439868246ddb1168fa42ea5b25
Alessandro Desantis <desa.alessandro@gmail.com>
2017-05-07 17:36:11 +0200

    Fix Ping operations

--- app/resources/api/v1/ping/operation/create.rb
+++ app/resources/api/v1/ping/operation/create.rb
@@ -3,11 +3,40 @@ module API
   module V1
     module Ping
       module Operation
-        class Create < Pragma::Operation::Create
-          protected
+        class Create < Pragma::Operation::Base
+          self['model.class'] = OpenStruct
 
-          def build_record
-            OpenStruct.new
+          step Pragma::Operation::Macro::Classes()
+          step Pragma::Operation::Macro::Model()
+          step Pragma::Operation::Macro::Policy()
+          failure :handle_unauthorized!, fail_fast: true
+          step Pragma::Operation::Macro::Contract::Build()
+          step Pragma::Operation::Macro::Contract::Validate()
+          failure :handle_invalid_contract!, fail_fast: true
+          step :sync!
+          step Pragma::Operation::Macro::Decorator()
+          step :respond!
+
+          def handle_unauthorized!(options)
+            options['result.response'] = Pragma::Operation::Response::Forbidden.new.decorate_with(
+              Pragma::Decorator::Error
+            )
+          end
+
+          def handle_invalid_contract!(options)
+            options['result.response'] = Pragma::Operation::Response::UnprocessableEntity.new(
+              errors: options['contract.default'].errors
+            ).decorate_with(Pragma::Decorator::Error)
+          end
+
+          def sync!(options)
+            options['contract.default'].sync
+          end
+
+          def respond!(options)
+            options['result.response'] = Pragma::Operation::Response::Created.new(
+              entity: options['result.decorator.default']
+            )
           end
         end
       end
