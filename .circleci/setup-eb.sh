90fbbf0956e1e0337cafc4be0e5af3a68dfd9160
Alessandro Desantis <desa.alessandro@gmail.com>
2018-06-09 15:28:58 +0200

    Remove EB deployment steps

--- .circleci/setup-eb.sh
+++
@@ -1,9 +0,0 @@
-set -x
-set -e
-
-mkdir ~/.aws
-touch ~/.aws/config
-chmod 600 ~/.aws/config
-echo "[profile eb-cli]" > ~/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
